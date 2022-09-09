d624b5eb9f08fcb90d6ac483f7e6806248b385bb
PiotrKrosniak <p.krosniak@gmail.com>
2022-09-09 14:30:07 +0200

    updated db configurations

--- hello/views.py
+++ hello/views.py
@@ -10,15 +10,6 @@ def index(request):
     return render(request, "index.html")
 
 
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
-
 def data_json(request):
     result_list = list(TableData.objects.all()\
                 .values('Index', 
